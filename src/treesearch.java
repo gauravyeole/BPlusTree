c6696e9a0a2737d4313141d3bb1006dd6dce99fd
Gaurav Yeole <gauravyeole@gmail.com>
2017-11-19 16:50:48 -0500

    Print in output_file.txt

--- src/treesearch.java
+++ src/treesearch.java
@@ -1,43 +1,9 @@
 import java.io.*;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Scanner;
+
 
 public class treesearch{
 	public static void main(String[] args){
 
-		// BPlusTree mytree = new BPlusTree(3);
-
-		// System.out.println("----------------Insert 1--------------------");
-		// mytree.insert(1,"val1");
-		// System.out.println("----------------Insert 3--------------------");
-		// mytree.insert(3,"val3");
-		// System.out.println("----------------Insert 5--------------------");
-		// mytree.insert(5,"val5");
-		
-		// System.out.println("----------------Insert 6--------------------");
-		
-		// mytree.insert(6,"val6");
-		// System.out.println("----------------Insert 9--------------------");
-		// mytree.insert(9,"val9");
-		// System.out.println("----------------Insert 16--------------------");
-		// mytree.insert(16,"val16");
-		// System.out.println("----------------Insert 17--------------------");
-		// mytree.insert(17,"val17");
-		// System.out.println("----------------Insert 30--------------------");
-		// mytree.insert(30,"val30");
-		// System.out.println("----------------Insert 40--------------------");
-		// mytree.insert(40,"val40");
-		// //mytree.insert(6,"val6B");
-		//  mytree.insert(1,"val1B");
-
-		
-		// mytree.search(9);
-		// mytree.search(5);
-		// mytree.search(2,7);
-		// mytree.search(3);
-		// mytree.search(2);
 
 		String filename = args[0];
 
@@ -46,6 +12,9 @@ public class treesearch{
 		try{
 			FileReader fileReader = new FileReader(filename);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			PrintStream fileOutput = new PrintStream(new File("output_file.txt"));
+
+			System.setOut(fileOutput);
 
 			line = bufferedReader.readLine();
 			int order = Integer.parseInt(line);
